f6e0f514677f436e711fd5ece359de348a2b2a31
energy-in-joles <42647510+energy-in-joles@users.noreply.github.com>
2018-10-13 12:30:08 +0800

    Add files via upload

---
+++ Single Service Queue Implementation + Statistics.py
@@ -0,0 +1,67 @@
+import time
+from random import randint
+from random import choice
+import numpy as np
+
+ar = []
+com = [0]
+wait = [0] 
+com_counter = 0
+ar_counter = 0
+opening_time = 28800 #time converted to seconds (0800 = 800 x 60 x 60)
+closing_time = 75600 
+
+wait_cap = 300 #generate percentage of customers who waited less than this duration (in seconds)
+
+def ar_clock(y):
+    n = time.strftime('%H:%M:%S', time.gmtime(y))
+    return n
+
+def ma_clock(z):
+    v = time.strftime('%H:%M:%S', time.gmtime(z))
+    return v
+
+arrival = randint(60,600) #gap between arrivals in seconds
+arrival += opening_time
+while arrival <= closing_time:
+    ar.append(arrival)
+    arrival += randint(60,600)
+
+else:
+    completion_time = randint(270,330) #time taken to complete activity
+    com.append((completion_time + ar[0]))
+    for x in range(1,len(ar)):
+        completion_time = randint(270,330)
+        if com[x] < ar[x]:
+            com.append((completion_time + ar[x]))
+            wait.append(0)
+        else:
+            com.append((completion_time + com[x]))
+            wait.append(com[x]-ar[x])
+            
+    com.remove(0)
+    com_ar = ar + com
+    while min(com_ar) < closing_time: #closing time used to displace used numbers
+        if com_ar.index(min(com_ar)) >= int(len(com_ar)/2):
+            com_counter += 1
+            print(ma_clock(min(com_ar)) + "  Customer %d has been served" % (com_counter))
+            com_ar[com_ar.index(min(com_ar))] = closing_time
+		
+        else:
+            ar_counter += 1
+            print(ar_clock(min(com_ar)) + "  Customer %d arrived." % (ar_counter))
+            com_ar[com_ar.index(min(com_ar))] = closing_time
+
+    else:
+        print("")
+        print("Average Waiting Time: " + str(round(np.mean(wait))) + "s")
+        counter = 0
+        for n in wait:
+            if n < wait_cap: 
+                counter += 1
+        print("Percentage of customers who waited 5 minutes or less: " + str(round((counter/len(wait) * 100), 1)) + "%")
+            
+        
+
+    
+    
